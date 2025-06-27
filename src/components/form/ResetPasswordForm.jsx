0b1b3ae6e59024c0defdf9807c85c35a05bc11b7
jerry <10166007@ntub.edu.tw>
2025-06-27 17:39:37 +0800

    feat：add password, text type conversion

--- src/components/form/ResetPasswordForm.jsx
+++ src/components/form/ResetPasswordForm.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useState, useContext } from 'react';
 import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { AuthContext } from '@/context/auth';
@@ -13,6 +13,8 @@ import { cn } from '@/utils/clsx';
 
 function ResetPasswordForm() {
   const { t } = useTranslation();
+
+  const [showPassword, setShowPassword] = useState(false);
   // const { showAlert } = useContext(AuthContext);
   const {
     register,
@@ -91,7 +93,7 @@ function ResetPasswordForm() {
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className='flex h-fit w-full flex-col gap-5'>
         {inputLists.map(({ name, placeholder, rules }, index) => (
-          <div key={name} className='flex h-fit w-full flex-col gap-2'>
+          <div key={name} className='relative flex h-fit w-full flex-col gap-2'>
             <div>
               <label className='flex-shrink-0 text-base' htmlFor={name}>
                 {rules?.required?.value && <span>*</span>}
@@ -100,7 +102,7 @@ function ResetPasswordForm() {
             </div>
             <input
               {...register(name, rules)}
-              type='password'
+              type={showPassword ? 'text' : 'password'}
               id={name}
               placeholder={placeholder}
               className={`focus:none h-11 w-full rounded border-2 ${errors?.[name] ? 'border-red-500' : 'border-grey-500'} bg-grey-700 indent-3 text-white focus:outline-none`}
@@ -110,6 +112,52 @@ function ResetPasswordForm() {
                 {errors?.[name]?.message}
               </p>
             )}
+            <button
+              type='button'
+              onClick={() => setShowPassword(!showPassword)}
+              className='absolute top-2/3 right-3 -translate-y-1/2 transform text-gray-400 hover:text-gray-600'
+            >
+              {showPassword ? (
+                <div>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    strokeWidth='1.5'
+                    stroke='currentColor'
+                    class='size-6'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      d='M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z'
+                    />
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      d='M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z'
+                    />
+                  </svg>
+                </div>
+              ) : (
+                <div>
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    strokeWidth='1.5'
+                    stroke='currentColor'
+                    class='size-6'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      d='M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88'
+                    />
+                  </svg>
+                </div>
+              )}
+            </button>
           </div>
         ))}
       </div>
